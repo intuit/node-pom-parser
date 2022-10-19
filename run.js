6e5cbf3614da06b997b5aa5ffc3b949fb360e976
kbhandari2 <kunal_bhandari@intuit.com>
2022-10-19 15:45:21 +0530

    added run.js as example run
    Signed-off-by: kbhandari2 <kunal_bhandari@intuit.com>

--- run.js
+++ run.js
@@ -1,10 +1,15 @@
 import parse from './lib/index.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 //var pomParser = require("./lib/index");
 // The required options, including the filePath.
 // Other parsing options from https://github.com/Leonidas-from-XIV/node-xml2js#options
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 var opts = {
-  filePath: __dirname + "/pom.xml", // The path to a pom file
+  filePath: __dirname + "/test/fixture/pom.xml", // The path to a pom file
 };
 // Parse the pom based on a path
 parse(opts, function(err, pomResponse) {
