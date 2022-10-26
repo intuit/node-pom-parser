5749fc5c73d0f2d21a64421a86495934916cb850
Sushil Pandey <SushilKumar_Pandey@intuit.com>
2022-10-26 11:57:56 +0530

    Fix #22: Adding code review comments

--- test/index.js
+++ test/index.js
@@ -1,30 +1,14 @@
-import parse from "../lib/index.js";
-import { expect } from "chai";
-import assert from "assert";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import parse from '../lib/index.js';
+import { expect } from 'chai';
+import assert from 'assert';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const POM_PATH = __dirname + "/fixture/pom.xml";
 
-//async functions are syntactic sugar for promises.
-//They never throw exceptions but may return rejected promise
-//Async function to handle the thrown exception from promise
-async function assertThrowsAsync(fn, regExp) {
-  let f = () => {};
-  try {
-    await fn();
-  } catch (e) {
-    f = () => {
-      throw e;
-    };
-  } finally {
-    assert.throws(f, regExp);
-  }
-}
-
 describe('require("pom-parser")', function () {
-  describe("loading from files", function () {
+  describe('loading from files', function () {
     let pomResponse = null;
     let pom = null;
     let xml = null;
@@ -52,13 +36,13 @@ describe('require("pom-parser")', function () {
       done();
     });
 
-    it("can load any pom.xml properly", function (done) {
+    it('can load any pom.xml properly', function (done) {
       expect(pomResponse.pomXml).to.be.an("string");
       expect(pomResponse.pomObject).to.be.an("object");
       done();
     });
 
-    it("parses xml attributes as properties", function (done) {
+    it('parses xml attributes as properties', function (done) {
       expect(pom.project.xmlns).to.equal("http://maven.apache.org/POM/4.0.0");
       expect(pom.project["xmlns:xsi"]).to.equal(
         "http://www.w3.org/2001/XMLSchema-instance"
@@ -66,35 +50,37 @@ describe('require("pom-parser")', function () {
       done();
     });
 
-    it("parses xml elements as properties", function (done) {
+    it('parses xml elements as properties', function (done) {
       expect(pom.project.parent).to.be.an("object");
       expect(pom.project.parent.artifactid).to.equal("tynamo-parent");
       done();
     });
   });
 
-  describe("when opts is null", function () {
-    it("parser should throw an error", async function () {
-      await assertThrowsAsync(
-        async () => await parse(null, function (err, response) {}),
-        /Error/
-      );
+  describe('when opts is null', function () {
+    it('parser should throw an error', async function () {
+      try {
+        await pomParser.parse(null, (err, result) => {});
+      } catch (e) {
+        expect(e).to.not.be.undefined;
+      }
     });
   });
 
-  describe("when opts is empty", function () {
-    it("parser should throw an error", async function () {
-      await assertThrowsAsync(
-        async () => await parse({}, function (err, response) {}),
-        /Error/
-      );
+  describe('when opts is empty', function () {
+    it('parser should throw an error', async function () {
+      try {
+        pomParser.parse({}, (err, result) => {});
+      } catch (e) {
+        expect(e).to.not.be.undefined;
+      }
     });
   });
 
-  describe("error scenarios", function () {
-    it("should return error if file does not exist", function (done) {
+  describe('error scenarios', function () {
+    it('should return error if file does not exist', function (done) {
       parse(
-        { filePath: __dirname + "incorrect-file-path" },
+        { filePath: __dirname + 'incorrect-file-path' },
         function (err, response) {
           expect(response).to.be.null;
           expect(err).to.not.be.null;
@@ -103,9 +89,9 @@ describe('require("pom-parser")', function () {
       );
     });
 
-    it("should return error if invalid xml file", function (done) {
+    it('should return error if invalid xml file', function (done) {
       parse(
-        { filePath: __dirname + "/fixture/pom2.xml" },
+        { filePath: __dirname + '/fixture/pom2.xml' },
         function (err, response) {
           expect(response).to.be.null;
           expect(err).to.not.be.null;
@@ -114,8 +100,8 @@ describe('require("pom-parser")', function () {
       );
     });
 
-    it("should return error if invalid xml content", function (done) {
-      const invalidXml = "<parent>this is invalid</PARENT>";
+    it('should return error if invalid xml content', function (done) {
+      const invalidXml = '<parent>this is invalid</PARENT>';
       parse({ xmlContent: invalidXml }, function (err) {
         expect(err).to.not.be.null;
         done();
--- test/xmlcontent.js
+++ test/xmlcontent.js
@@ -1,24 +1,24 @@
-import parse from "../lib/index.js";
-import { expect } from "chai";
+import parse from '../lib/index.js';
+import { expect } from 'chai';
 
 let POM_CONTENT =
   '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
   '    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">' +
-  "  <parent>" +
-  "    <artifactId>tynamo-parent</artifactId>" +
-  "    <groupId>org.tynamo</groupId>" +
-  "    <version>0.0.9</version>" +
-  "  </parent>" +
+  '  <parent>' +
+  '    <artifactId>tynamo-parent</artifactId>' +
+  '    <groupId>org.tynamo</groupId>' +
+  '    <version>0.0.9</version>' +
+  '  </parent>' +
   '  <modelVersion parallel="now">4.0.0</modelVersion>' +
-  "  <groupId>org.tynamo.examples</groupId>" +
-  "  <artifactId>tynamo-example-federatedaccounts</artifactId>" +
-  "  <version>0.0.1-SNAPSHOT</version>" +
-  "  <packaging>war</packaging>" +
-  "  <name>Tynamo Example - Federated Accounts</name>" +
-  "</project>";
+  '  <groupId>org.tynamo.examples</groupId>' +
+  '  <artifactId>tynamo-example-federatedaccounts</artifactId>' +
+  '  <version>0.0.1-SNAPSHOT</version>' +
+  '  <packaging>war</packaging>' +
+  '  <name>Tynamo Example - Federated Accounts</name>' +
+  '</project>';
 
 describe('require("pom-parser") using xml content', function () {
-  describe("loading from strings", function () {
+  describe('loading from strings', function () {
     let pomResponse = null;
     let pom = null;
 
@@ -43,12 +43,12 @@ describe('require("pom-parser") using xml content', function () {
       done();
     });
 
-    it("can parse any pom xml in strings properly", function (done) {
+    it('can parse any pom xml in strings properly', function (done) {
       expect(pomResponse.pomObject).to.be.an("object");
       done();
     });
 
-    it("parses xml attributes as properties", function (done) {
+    it('parses xml attributes as properties', function (done) {
       expect(pom.project.xmlns).to.equal("http://maven.apache.org/POM/4.0.0");
       expect(pom.project["xmlns:xsi"]).to.equal(
         "http://www.w3.org/2001/XMLSchema-instance"
@@ -56,7 +56,7 @@ describe('require("pom-parser") using xml content', function () {
       done();
     });
 
-    it("parses xml elements as properties", function (done) {
+    it('parses xml elements as properties', function (done) {
       expect(pom.project.parent).to.be.an("object");
       expect(pom.project.parent.artifactid).to.equal("tynamo-parent");
       done();
